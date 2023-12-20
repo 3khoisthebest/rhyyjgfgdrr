f43c92333c569d8fe6dccdbfea8775dbddd65682
3khoisthebest <aaqilnazeem.mohammedibrahim.404@k12.friscoisd.org>
2023-12-20 15:19:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ public/index.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  window.navigator.serviceWorker.register('./sw.js', {
+    scope: __uv$config.prefix
+  }).then(() => {
+    var selectedSearchEngine = localStorage.getItem("selectedSearchEngine");
+    var searchEngine = input.getAttribute("data-search-engine");
+    var url = input.value.trim();
+    if (!isUrl(url)) url = (searchEngine || selectedSearchEngine) + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+    localStorage.setItem('iframeurl', __uv$config.prefix + __uv$config.encodeUrl(url))
+    if (window.location.href.includes('/drive.html')) {
+      var iframe = document.getElementById('iframeurl');
+      var storedUrl = localStorage.getItem('iframeurl');
+      if (storedUrl) {
+        iframe.src = storedUrl; // Set the src attribute of the iframe
+      }
+      // ...
+    } else {
+      window.location.href = "./drive.html"; // Redirect to /drive.html
+    }
+    
+  });
+});
+
+function isUrl(val = '') {
+  if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+  return false;
+};
+
+
+function openURL(url) {
+  window.navigator.serviceWorker
+  .register("./sw.js", {
+    scope: __uv$config.prefix,
+  })
+  .then(() => {
+    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+    else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "http://" + url;
+       localStorage.setItem('iframeurl', __uv$config.prefix + __uv$config.encodeUrl(url))
+          window.location.href = "./drive.html";
+
+  });
+};
+
+
+function openGame(url) {
+      window.location.href = url;
+};
